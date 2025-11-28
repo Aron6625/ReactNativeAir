285f587f7e452939baceb1ae19dd8adf79b0a80d
Aron <aronpad123@gmail.com>
2025-11-27 20:18:07 -0400

    installs implementations and atomic

--- src/screen/WelcomeScreen.tsx
+++ src/screen/WelcomeScreen.tsx
@@ -1,13 +1,22 @@
-import { Text, StyleSheet} from "react-native"
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Text, StyleSheet, Image} from "react-native"
+import { SafeAreaProvider ,SafeAreaView} from "react-native-safe-area-context";
+import TextComponent from "../components/TextComponent";
 
 const WelcomenScreen = () => (
     <SafeAreaProvider style={styles.container}>
-        <Text>biegkjknvenido
-            <Text style={styles.textNegrilla} 
+      <SafeAreaView>
+
+        <TextComponent size="16" weight="semibold" color="dark">biegkjknvenido
+            <TextComponent  size="24" weight="light" color="primary" textAlign="right" underline={true}
             onPress={ () => console.log('test')}>
-                hola mundo</Text>
-        </Text>
+                hola mundo</TextComponent>
+        </TextComponent>
+        <Image
+        style={{width: 200, height: 200}}
+          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}>
+        </Image>
+      </SafeAreaView>
+
     </SafeAreaProvider>
 );
 const styles = StyleSheet.create({
